e9ecdcf6314e81a334328aafc4a29e44c118873d
Eden Gaon <edencgaon@gmail.com>
2025-03-01 20:05:14 +0200

    Small fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,8 @@ export const metadata: Metadata = {
   description: 'In the Soup for Love',
   generator: 'EdenG',
   icons: {
-    icon: [
-      { url: '/favicon.ico', sizes: 'any' },
-      { url: '/icon/icon.webp', type: 'image/webp' }
-    ],
-    shortcut: [{ url: '/favicon.ico' }],
-    apple: [{ url: '/icon/icon.webp' }]
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
   }
 }
 
@@ -22,11 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/icon/icon.webp" />
-      </head>
       <body>{children}</body>
     </html>
   )
